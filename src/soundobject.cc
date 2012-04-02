c98a55d9ce974d3153004d7af22f7ce1ab899ae3
Yao Li <hnkfliyao@gmail.com>
2012-04-02 23:46:25 +0800

    Sound. Something missed.

---
+++ src/soundobject.cc
@@ -0,0 +1,31 @@
+#include <QtCore/QCoreApplication>
+#include <QtGui/QSound>
+#include "soundobject.hh"
+
+using namespace std;
+
+SoundObject::SoundObject(char* src)
+    : QObject(), m_src(src)
+{
+#ifdef USE_PHONON
+#endif
+}
+
+SoundObject::~SoundObject()
+{
+#ifdef USE_PHONON
+    delete m_phononMedia;
+#endif
+}
+
+void SoundObject::play(void)
+{
+#ifdef USE_PHONON
+    m_phononMedia = Phonon::createPlayer(Phonon::MusicCategory,
+                                         Phonon::MediaSource(m_src));
+    m_phononMedia->play();
+#endif
+#ifdef USE_QSOUND
+    QSound::play(m_src);
+#endif
+}
